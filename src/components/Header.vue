b6ea616cbb7719b9f720219892b395a21e1678ad
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-11 11:53:31 +0800

    feat: Drafted a header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <div class="header__logo">Assets Library</div>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+.header
+  height: 80px
+  padding: 0 20%
+  display: flex
+  justify-content: center
+  align-items: center
+  border-bottom: 1px solid #eee
+  box-shadow: 1px 1px 10px #eee
+
+  &__logo
+    font-size: 1.5em
+</style>
