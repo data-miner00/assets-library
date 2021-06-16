a3394dfb4e883362d835b63a176b6e4517c98b75
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-16 23:23:25 +0800

    feat: Added showcase page

---
+++ src/views/Showcase.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="showcase">
+    <section class="showcase__section">
+      <h1>Card Reflect Hover</h1>
+      <CardReflectHover />
+    </section>
+    <section class="showcase__section">
+      <h1>Animating Gradient Background</h1>
+      <AnimatingGradient />
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import CardReflectHover from "@/ui-components/others/CardReflectHover/index.vue";
+import AnimatingGradient from "@/ui-components/others/AnimatingGradient.vue";
+
+export default Vue.extend({
+  components: {
+    CardReflectHover,
+    AnimatingGradient,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.showcase
+  padding: 100px 0
+  &__section
+    padding: 50px 0
+    h1
+      text-align: center
+      margin-bottom: 50px
+</style>
