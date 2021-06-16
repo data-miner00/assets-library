04a0c4bc40c4b9ae6eaade1fee875f955e8a87cb
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-16 23:22:48 +0800

    feat: Added some home content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <h4>This is heading 4</h4>
+    <h5>This is heading 5</h5>
+    <h6>This is heading 6</h6>
+    <p>
+      This is a paragraph Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Dolorum, debitis quasi eligendi est consectetur culpa molestiae at
+      voluptatibus, distinctio assumenda sit repellat unde obcaecati, natus aut
+      error et quae cum!
+    </p>
+    <button>hello</button>
+    <hr />
+    <blockquote>
+      This is a blockquote Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Commodi perspiciatis minus magni placeat, ipsum earum. Delectus
+      natus expedita numquam sapiente cupiditate harum quos fugit praesentium
+      dolores? Error maxime dolorem doloribus!
+    </blockquote>
+    <textarea name="display" id="" cols="30" rows="10">
+fsdfsdfsdfasfsfasdfsadf</textarea
+    >
+    <input type="text" placeholder="This is a input" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 });
 </script>
+
+<style lang="sass" scoped></style>
