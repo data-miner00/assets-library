3787b15ee2cbf6ba1f8644e9e70629b722945444
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-16 23:42:35 +0800

    feat: Added loader

---
+++ src/ui-components/loaders/BoxRotating.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="box-rotating">
+    <div class="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box-rotating {
+  background: #262626;
+  height: 100vh;
+}
+.loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(45deg);
+  width: 100px;
+  height: 100px;
+  background: #262626;
+  animation: animate 1s linear infinite;
+}
+@keyframes animate {
+  0% {
+    width: 100px;
+    height: 100px;
+  }
+  10% {
+    width: 100px;
+    height: 100px;
+  }
+  50% {
+    width: 150px;
+    height: 150px;
+  }
+  90% {
+    width: 100px;
+    height: 100px;
+  }
+  100% {
+    width: 100px;
+    height: 100px;
+  }
+}
+.loader span {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background: #f00;
+  animation: rotate 1s linear infinite;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  10% {
+    transform: rotate(0deg);
+  }
+  60% {
+    transform: rotate(90deg);
+  }
+  90% {
+    transform: rotate(90deg);
+  }
+  100% {
+    transform: rotate(90deg);
+  }
+}
+.loader span:nth-child(1) {
+  top: 0;
+  left: 0;
+  background: #f79f1f;
+}
+.loader span:nth-child(2) {
+  top: 0;
+  right: 0;
+  background: #12cbc4;
+}
+.loader span:nth-child(3) {
+  bottom: 0;
+  left: 0;
+  background: #ed4c67;
+}
+.loader span:nth-child(4) {
+  bottom: 0;
+  right: 0;
+  background: #a3cb38;
+}
+</style>
