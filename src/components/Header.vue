fee2c5a65d9fa2468527ef3e46004daadfe84614
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-16 23:21:38 +0800

    feat: Better header with working links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,19 @@
 <template>
   <header class="header">
     <div class="header__logo">Assets Library</div>
+    <nav class="header__nav">
+      <router-link to="showcase">
+        <div class="header__nav__item">Showcase</div>
+      </router-link>
+
+      <router-link to="library">
+        <div class="header__nav__item">Library</div>
+      </router-link>
+
+      <router-link to="editor">
+        <div class="header__nav__item">Editor</div>
+      </router-link>
+    </nav>
   </header>
 </template>
 
@@ -9,11 +22,21 @@
   height: 80px
   padding: 0 20%
   display: flex
-  justify-content: center
+  justify-content: space-between
   align-items: center
   border-bottom: 1px solid #eee
   box-shadow: 1px 1px 10px #eee
+  position: sticky
+  top: 0
+  left: 0
+  width: 100%
+  background: white
 
   &__logo
     font-size: 1.5em
+
+  &__nav
+    width: 200px
+    justify-content: space-between
+    display: flex
 </style>
