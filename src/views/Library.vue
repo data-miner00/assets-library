5884dc99850ae7e030f4586b542c2b620cd3bd56
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-11 11:37:05 +0800

    feat: Added library page

---
+++ src/views/Library.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="library">
+    <h1>Useful utility website for design</h1>
+    <a href="https://loremipsum.io/" class="utility">
+      <h3>Lorem Ipsum</h3>
+      <p>https://loremipsum.io/</p>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  //
+});
+</script>
+
+<style lang="sass" scoped></style>
