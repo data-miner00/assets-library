1580fe2a07142278feaafed7a87a941cee45d249
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-15 20:03:01 +0800

    refactor: Externalize font awesome

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faUserSecret);
-
-Vue.component("Fa", FontAwesomeIcon);
+import "./plugins/fontAwesome";
 
 Vue.config.productionTip = false;
 
