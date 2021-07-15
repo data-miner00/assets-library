5a311b87c6dd8c6d7d883966d7a5abaa13ee572d
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-15 20:02:28 +0800

    chore: Clean unused codes

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
 <template>
   <div id="app">
-    <Header></Header>
     <router-view />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import Header from "./components/Header.vue";
-export default Vue.extend({
-  components: { Header },
-});
-</script>
 
 <style lang="sass">
 @import "./assets/sass/global"
 
 #app
   position: relative
-
-// Archive
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-
-// #nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
