42212b2d78825259e48a875c9998eff1a94c6496
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-15 20:42:49 +0800

    chore: Add index sass

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/fontAwesome";
+import "./index.sass";
 
 Vue.config.productionTip = false;
 
