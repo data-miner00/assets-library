c6d295f0903d9dfbb4c59424063c0af5395348f0
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-16 10:38:16 +0800

    fix: Fixed router link misbehaviour

--- src/views/Documentation/index.vue
+++ src/views/Documentation/index.vue
@@ -1,52 +1,54 @@
 <template>
   <div class="docs">
-    <nav class="docs__nav">
-      <router-link to="">
-        <div class="docs__nav__link">
-          <span>Getting Started</span>
-        </div>
-      </router-link>
-      <router-link to="html">
-        <div class="docs__nav__link">
-          <span>HTML Elements</span>
-        </div>
-      </router-link>
-      <router-link to="buttons">
-        <div class="docs__nav__link">
-          <span>Buttons</span>
-        </div>
-      </router-link>
-      <router-link to="loaders">
-        <div class="docs__nav__link">
-          <span>Loaders</span>
-        </div>
-      </router-link>
-      <router-link to="fonts">
-        <div class="docs__nav__link">
-          <span>Fonts</span>
-        </div>
-      </router-link>
-      <router-link to="frameworks">
-        <div class="docs__nav__link">
-          <span>Frameworks</span>
-        </div>
-      </router-link>
-      <router-link to="assets">
-        <div class="docs__nav__link">
-          <span>Assets</span>
-        </div>
-      </router-link>
-      <router-link to="palettes">
-        <div class="docs__nav__link">
-          <span>Color Palettes</span>
-        </div>
-      </router-link>
-      <router-link to="wysiwyg">
-        <div class="docs__nav__link">
-          <span>WYSIWYG</span>
-        </div>
-      </router-link>
-    </nav>
+    <div class="wrap">
+      <nav class="docs__nav">
+        <router-link :to="{ name: 'getting-started' }">
+          <div class="docs__nav__link">
+            <span>Getting Started</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'html-elements' }">
+          <div class="docs__nav__link">
+            <span>HTML Elements</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'buttons' }">
+          <div class="docs__nav__link">
+            <span>Buttons</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'loaders' }">
+          <div class="docs__nav__link">
+            <span>Loaders</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'fonts' }">
+          <div class="docs__nav__link">
+            <span>Fonts</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'frameworks' }">
+          <div class="docs__nav__link">
+            <span>Frameworks</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'assets' }">
+          <div class="docs__nav__link">
+            <span>Assets</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'color-palettes' }">
+          <div class="docs__nav__link">
+            <span>Color Palettes</span>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'wysiwyg' }">
+          <div class="docs__nav__link">
+            <span>WYSIWYG</span>
+          </div>
+        </router-link>
+      </nav>
+    </div>
     <main class="docs__content">
       <router-view></router-view>
     </main>
@@ -59,14 +61,21 @@
   color: #996ed0
 
 .docs
-  padding: 0 20%
+  padding: 0 16%
   display: flex
 
-  &__nav
-    padding: 2rem 0
-    border-right: 1px solid #eee
+  .wrap
     height: calc(100vh - 65px)
     flex-basis: 300px
+    flex-shrink: 0
+  &__nav
+    border-right: 1px solid #eee
+    padding: 2rem 0
+    width: 300px
+    height: 100%
+    position: fixed
+    top: 65px
+
 
     &__link
       margin: 5px 20px 5px 0
