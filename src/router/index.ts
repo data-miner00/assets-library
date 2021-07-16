bd8565662008099e8bea0df4f9a75a775e2de7a0
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-16 08:37:18 +0800

    feat: Added paths for docs child

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,12 +7,12 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
     path: "/about",
-    name: "About",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -21,27 +21,91 @@ const routes: Array<RouteConfig> = [
   },
   {
     path: "/docs",
-    name: "Documentation",
+    name: "documentation",
     component: () =>
-      import(
-        /* webpackChunkName: "editor" */ "../views/Documentation/index.vue"
-      ),
+      import(/* webpackChunkName: "docs" */ "../views/Documentation/index.vue"),
+    children: [
+      {
+        path: "",
+        name: "getting-started",
+        component: () =>
+          import(
+            /* webpackChunkName: "getting" */ "../views/Documentation/Getting.vue"
+          ),
+      },
+      {
+        path: "html",
+        name: "html-elements",
+        component: () =>
+          import(
+            /* webpackChunkName: "html" */ "../views/Documentation/Html.vue"
+          ),
+      },
+      {
+        path: "buttons",
+        name: "buttons",
+        component: () =>
+          import(
+            /* webpackChunkName: "buttons" */ "../views/Documentation/Buttons.vue"
+          ),
+      },
+      {
+        path: "loaders",
+        name: "loaders",
+        component: () =>
+          import(
+            /* webpackChunkName: "loaders" */ "../views/Documentation/Loaders.vue"
+          ),
+      },
+      {
+        path: "frameworks",
+        name: "frameworks",
+        component: () =>
+          import(
+            /* webpackChunkName: "frameworks" */ "../views/Documentation/Frameworks.vue"
+          ),
+      },
+      {
+        path: "assets",
+        name: "assets",
+        component: () =>
+          import(
+            /* webpackChunkName: "assets" */ "../views/Documentation/Assets.vue"
+          ),
+      },
+      {
+        path: "palettes",
+        name: "color-palettes",
+        component: () =>
+          import(
+            /* webpackChunkName: "color-palettes" */ "../views/Documentation/Color.vue"
+          ),
+      },
+      {
+        path: "wysiwyg",
+        name: "wysiwyg",
+        component: () =>
+          import(
+            /* webpackChunkName: "wysiwyg" */ "../views/Documentation/Wysiwyg.vue"
+          ),
+      },
+    ],
   },
   {
     path: "/editor",
-    name: "Editor",
+    name: "editor",
     component: () =>
       import(/* webpackChunkName: "editor" */ "../views/Editor.vue"),
   },
   {
     path: "/showcase",
-    name: "Showcase",
+    name: "showcase",
     component: () =>
       import(/* webpackChunkName: "showcase" */ "../views/Showcase.vue"),
   },
   {
     path: "/library",
-    name: "Library",
+    name: "library",
     component: () =>
       import(/* webpackChunkName: "library" */ "../views/Library.vue"),
   },
---
+++ src/views/Documentation/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="docs">
+    <nav class="docs__nav">
+      <router-link to="">
+        <div class="docs__nav__link">
+          <span>Getting Started</span>
+        </div>
+      </router-link>
+      <router-link to="html">
+        <div class="docs__nav__link">
+          <span>HTML Elements</span>
+        </div>
+      </router-link>
+      <router-link to="buttons">
+        <div class="docs__nav__link">
+          <span>Buttons</span>
+        </div>
+      </router-link>
+      <router-link to="loaders">
+        <div class="docs__nav__link">
+          <span>Loaders</span>
+        </div>
+      </router-link>
+      <router-link to="fonts">
+        <div class="docs__nav__link">
+          <span>Fonts</span>
+        </div>
+      </router-link>
+      <router-link to="frameworks">
+        <div class="docs__nav__link">
+          <span>Frameworks</span>
+        </div>
+      </router-link>
+      <router-link to="assets">
+        <div class="docs__nav__link">
+          <span>Assets</span>
+        </div>
+      </router-link>
+      <router-link to="palettes">
+        <div class="docs__nav__link">
+          <span>Color Palettes</span>
+        </div>
+      </router-link>
+      <router-link to="wysiwyg">
+        <div class="docs__nav__link">
+          <span>WYSIWYG</span>
+        </div>
+      </router-link>
+    </nav>
+    <main class="docs__content">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.active div
+  background: #f5ecff
+  color: #996ed0
+
+.docs
+  padding: 0 20%
+  display: flex
+
+  &__nav
+    padding: 2rem 0
+    border-right: 1px solid #eee
+    height: calc(100vh - 65px)
+    flex-basis: 300px
+
+    &__link
+      margin: 5px 20px 5px 0
+      padding: 15px 15px
+      font-size: 1.3em
+      border-radius: 5px
+      transition: background .2s, color .2s
+
+      &:hover
+        background: #f5ecff
+        color: #996ed0
+
+  &__content
+    padding: 2rem 0 2rem 2rem
+    min-height: 200vh
+    flex-grow: 1
+</style>
