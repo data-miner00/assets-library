7e545bcf9929c15fe5752da88be798c862854845
data-miner00 <data-miner00@users.noreply.github.com>
2021-08-27 23:59:35 +0800

    wip: Content navigation box

---
+++ src/components/TableContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="table">
+    <div class="table__title">
+      <span>Contents</span>
+    </div>
+    <div class="table__contents">
+      <div class="table__contents__item">
+        <span>Introduction</span>
+      </div>
+      <div class="table__contents__item">
+        <span>Why Assets Library</span>
+      </div>
+      <div class="table__contents__item">
+        <span>The Plight</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.table
+  background: #eee
+  &__title
+    font-size: 1.15em
+    font-weight: 600
+    margin-bottom: 20px
+  &__contents
+    padding: 0 20px
+
+    &__items
+      padding: 0 20px 0 40px
+      margin: 20px 0
+</style>
