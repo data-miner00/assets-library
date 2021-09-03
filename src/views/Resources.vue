df9ba43c0fb96f957ca5411945feb83bd89b8579
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-03 08:36:40 +0800

    feat: Collected resources

---
+++ src/views/Resources.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="resources">
+    <h1>Resources</h1>
+  </div>
+</template>
+
+<!-- 
+Guidelines
+https://material.io/
+https://developer.apple.com/design/human-interface-guidelines/
+
+https://bulma.io/
+https://vuetifyjs.com/en/
+https://material.angular.io/
+https://getbootstrap.com/
+https://tailwindcss.com/
+https://materializecss.com/
+https://loremipsum.io/
+https://picsum.photos/
+https://www.awwwards.com/
+https://cssnectar.com/
+https://www.siteinspire.com/
+https://coolors.co/
+https://fonts.google.com/
+-->
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  //
+});
+</script>
+
+<style lang="sass" scoped>
+.resources
+  padding: 0 16%
+  h1
+    color: purple
+</style>
